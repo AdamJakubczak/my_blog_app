81947c78e3473eff51da1d53d19f93a48293cef7
Adam Jakubczak <a.jakubczak@hotmail.com>
2024-08-16 22:23:47 +0200

    Logging in and logging out, DONE!

--- src/my_blog_app/urls.py
+++ src/my_blog_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='blog_index' ),
+    path('login/', views.user_login, name='blog_login'),
+    path('logout/', views.user_logout, name='blog_logout' ),
 ]
--- src/my_blog_app/views.py
+++ src/my_blog_app/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import login, authenticate, logout
+
 
 def index(request):
-    return HttpResponse('Hello world!')
+    
+    return render(request, 'my_blog_app/index.html')
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('blog_index')
+    
+    return render(request, 'my_blog_app/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('blog_index')
